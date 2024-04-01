32bf83293f3271b9ba63065d04588a83aaa4ffcb
AgiriFeyisara <mohrenny8@gmail.com>
2024-04-01 13:59:27 +0100

    Updated the Landing-Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import "./App.css";
-import { Header } from "./component/LandingPage/Header";
-import { Hero } from "./component/LandingPage/Hero";
-import { Icons } from "./component/LandingPage/Icons";
-import { Stats } from "./component/LandingPage/Stats";
-import { Profile } from "./component/LandingPage/Profile";
-import { UpdatedNews } from "./component/LandingPage/UpdatedNews";
-import { Footer } from "./component/LandingPage/Footer";
+
+import LandingPage from "./component/LandingPage/LandingPage.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <Icons />
-      <Stats />
-      <Profile />
-      <UpdatedNews />
-      <Footer />
-    </div>
-  );
+  return <LandingPage />;
 }
 
 export default App;
