37f635379ca9b687a5ccbbc8426310e6f3d06a9e
Codegirlemem <eduokuemem@gmail.com>
2024-04-22 23:35:36 +0100

    new commit

--- src/hooks/useInputValues.js
+++ src/hooks/useInputValues.js
@@ -14,6 +14,8 @@ export function useInputValues(account, signup) {
   validateInput(inputValue, signup);
   const [isValid, invalidValues] = validateInput(inputValue, signup);
 
+  console.log(setinputErrors, invalidValues);
+
   // console.log(invalidValues, setinputErrors);
   // useEffect(function () {}, [inputValue]);
   return [inputValue, inputErrors, isValid, handleInputChange, setInputValue];
