0741631dd27e474706e347b64913faf4a12b4c77
Codegirlemem <eduokuemem@gmail.com>
2024-06-04 11:00:44 +0100

    created cards for content categories

--- src/components/dashboard/content-bank/ContentSeries.jsx
+++ src/components/dashboard/content-bank/ContentSeries.jsx
@@ -1,5 +1,12 @@
+import CtaButtons from './atom/CtaButtons';
+
 function ContentSeries() {
-  return <div>Content Series</div>;
+  return (
+    <div className="">
+      <p className="mb-16">Coming Soon!</p>
+      <CtaButtons />
+    </div>
+  );
 }
 
 export default ContentSeries;
